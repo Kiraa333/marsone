513bdaa0f5c762cf3507d05c10ffbf47c5cbc0a9
Kira <kira.jeda@gmail.com>
2025-03-27 14:23:39 +0300

    login 2

--- data/users.py
+++ data/users.py
@@ -1,8 +1,11 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
@@ -17,3 +20,8 @@ class User(SqlAlchemyBase):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                       default=datetime.datetime.now)
+
+
+    def __repr__(self):
+        return f'<User>{self.id} {self.name} {self.email}'
+
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, redirect
+from flask_login import login_user
+from flask_login import LoginManager
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
-from data import login
+from data import register
+from data.login import LoginForm
+from data.register import RegisterForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
 
 @app.route('/')
 @app.route('/works')
@@ -16,9 +29,25 @@ def works():
     jobs = session.query(Jobs)
     return render_template('jobs.html', jobs=jobs)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.hashed_password == form.password.data:
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = login.RegisterForm()
+    form = RegisterForm()
     db_session.global_init("db/mars_explorer.db")
     session = db_session.create_session()
     if form.validate_on_submit():
