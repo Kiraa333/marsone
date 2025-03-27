145f41e3e172107581df0ff7758fa4802b1efc88
Kira <kira.jeda@gmail.com>
2025-03-27 17:40:25 +0300

    login 3

---
+++ data/register.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.fields.simple import EmailField
+from wtforms.validators import DataRequired
+
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Логин / Почта', validators=[DataRequired()])
+    password1 = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    age = StringField('Возраст', validators=[DataRequired()])
+    position = StringField('Должность', validators=[DataRequired()])
+    speciality = StringField('Специальность', validators=[DataRequired()])
+    address = StringField('Адрес', validators=[DataRequired()])
+    submit = SubmitField('Зарегестрироваться')
